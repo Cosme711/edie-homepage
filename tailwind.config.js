b6ab62b02a2956d77581ccf1feb81a386c35955b
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-11 14:48:50 +0100

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
       'heebo': ['Heebo'],
       'montserrat': ['Montserrat'],
       'poppins': ['Poppins']
+    },
+    colors: {
+      blue: '#2D9CDB',
+      green: '#27AE60',
+      red: '#EB5757'
+    },
+    textColor: {
+      'gray': '#828282',
+      'darkgray': '#333333',
+      'whitegray': '#A9A9A9',
+      'blue': '#2D9CDB'
     }
   },
   variants: {},
