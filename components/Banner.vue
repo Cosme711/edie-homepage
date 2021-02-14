0d298f805d4e338a7b39d7ef4dbbaff33f5d2d93
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-14 18:16:57 +0100

    Update Banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,19 +1,21 @@
 <template>
-  <section class="mt-12">
-      <div class="px-4">
-        <h4 class="text-blue text-sm font-medium">Unhappy with your website?</h4>
-        <h2 class="mt-1 pr-4 text-darkgray text-2xl font-medium">We create beautiful and fast web services</h2>
+  <section class="mt-12 lg:mt-16">
+      <div class="px-4 lg:px-0 lg:w-7/12 2xl:w-5/12 lg:m-auto">
+        <h4 class="text-blue text-sm lg:text-base font-medium">Unhappy with your website?</h4>
+        <h2 class="mt-1 pr-4 text-darkgray text-2xl lg:text-4xl lg:pr-24 xl:pr-0 font-medium">We create beautiful and fast web services</h2>
       </div>
-      <img src="../assets/images/illustrations/heroImage.jpg" alt="Hero Image" class="mt-4 h-40 object-cover rounded-3xl">
-      <div class="px-6 mt-6">
-          <h3 class="pr-16 text-2xl text-darkgray font-medium">Story, emotion and purpose</h3>
+      <div class="lg:px-10">
+        <img src="../assets/images/illustrations/heroImage.jpg" alt="Hero Image" class="m-auto mt-4 md:mt-10  h-40 sm:h-full object-cover rounded-2xl">
+      </div>
+      <div class="px-6 lg:px-0 lg:w-7/12 2xl:w-5/12 lg:m-auto pt-6 lg:pt-10">
+        <div class="lg:w-2/4">
+          <h3 class="text-2xl lg:text-4xl text-darkgray font-medium">Story, emotion and purpose</h3>
           <p class="mt-4 text-whitegray text-sm">We help transform your ideas into real world applications that will outperform your toughest competition and help you achieve your strategic goals in short period of time.</p>
-      </div>
-      <div class="px-6 mt-10">
-        <p class="text-gray text-sm">Want us to contact you?</p>
-        <div class="mt-1 bg-input flex rounded-xl">
-          <input class="py-4 pl-6 pr-1 w-full bg-input outline-none rounded-xl" type="text" placeholder="Email"/>
-          <a href="" class="my-2 mr-2 px-4 flex items-center text-white bg-blue rounded-xl">Join</a>
+          <p class="mt-10 text-gray text-sm">Want us to contact you?</p>
+          <div class="mt-1 bg-input flex rounded-xl">
+            <input class="py-4 pl-6 pr-1 w-full bg-input outline-none rounded-xl" type="text" placeholder="Email"/>
+            <a href="" class="my-2 mr-2 px-4 flex items-center text-white bg-blue rounded-xl">Join</a>
+          </div>
         </div>
       </div>
   </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: {
     screens: {
       'mobile': '600px',
+      'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
