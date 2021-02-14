f781c64ddb5cfac4ad140b01de104443c5934ca1
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-14 19:41:47 +0100

    Update Teams

--- components/Teams.vue
+++ components/Teams.vue
@@ -1,15 +1,19 @@
 <template>
     <section class="mt-16">
-        <p class="font-medium text-red">Meet the team</p>
-        <h3 class="mt-4 pr-20 font-medium text-darkgray text-3xl">We are chilled and a laidback team</h3>
-        <p class="pr-20 mt-3 text-whitegray text-sm">Lorem ipsum dolor sit amet, consecteur adipiscing elit.</p>
-        <div class="flex items-center">
-            <div class="pr-2">
-                <img class="rounded-3xl" src="../assets/images/profiles/person3.png" alt="">
+        <div class="lg:flex lg:items-center lg:w-10/12 xl:w-8/12 lg:m-auto">
+            <div>
+                <p class="font-medium text-red">Meet the team</p>
+                <h3 class="mt-4 pr-20 font-medium text-darkgray text-3xl">We are chilled and a laidback team</h3>
+                <p class="pr-20 mt-3 text-whitegray text-sm">Lorem ipsum dolor sit amet, consecteur adipiscing elit.</p>
             </div>
-            <div class="grid gap-2 grid-rows-2">
-                <img class="w-11/12 mt-auto rounded-3xl" src="../assets/images/profiles/person1.png" alt="">
-                <img class="rounded-3xl" src="../assets/images/profiles/person2.png" alt="">
+            <div class="flex items-center">
+                <div class="pr-2">
+                    <img class="rounded-3xl" src="../assets/images/profiles/person3.png" alt="">
+                </div>
+                <div class="grid gap-2 grid-rows-2">
+                    <img class="w-11/12 mt-auto rounded-3xl" src="../assets/images/profiles/person1.png" alt="">
+                    <img class="rounded-3xl" src="../assets/images/profiles/person2.png" alt="">
+                </div>
             </div>
         </div>
         <p class="mt-20 text-2xl text-darkgray font-medium">“Fast and outstanding resutls. Edie understands their customer’s needs. They have a young and talented team.”</p>
