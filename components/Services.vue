17a5295b3b197ea52ceea98b466ba39349840c73
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-15 17:07:33 +0100

    Update Hover on Services

--- components/Services.vue
+++ components/Services.vue
@@ -2,24 +2,31 @@
     <section class="mt-24 px-4 xl:mt-40 xl:w-8/12 xl:m-auto">
         <h3 class="text-darkgray font-medium mobile:text-lg sm:text-xl lg:text-2xl">We offer high demand services</h3>
         <div class="md:grid md:grid-cols-3 md:gap-5 xl:gap-16">
-            <div class="mt-16 md:mt-24 max-w-lg">
+            <div class="parent mt-16 p-6 md:mt-24 max-w-lg hover:shadow-2xl rounded-xl">
                 <span class="material-icons p-4 text-3xl bg-blue text-white rounded-xl">create</span>
                 <h3 class="pt-6 text-2xl text-darkgray font-bold">UI/UX Design</h3>
                 <p class="mt-5 text-whitegray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</p>
-                <a class="py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
+                <a class="button py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
             </div>
-            <div class="mt-24 max-w-lg">
+            <div class="parent mt-24 p-6 max-w-lg hover:shadow-2xl rounded-xl">
                 <span class="material-icons p-4 text-3xl bg-green text-white rounded-xl">code</span>
                 <h3 class="pt-6 text-2xl text-darkgray font-bold">Front End</h3>
                 <p class="mt-5 text-whitegray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</p>
-                <a class="py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
+                <a class="button py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
             </div>
-            <div class="mt-24 max-w-lg">
+            <div class="parent mt-24 p-6 max-w-lg hover:shadow-2xl rounded-xl">
                 <span class="material-icons p-4 text-3xl bg-red text-white rounded-xl">storage</span>
                 <h3 class="pt-6 text-2xl text-darkgray font-bold">Back End</h3>
                 <p class="mt-5 text-whitegray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</p>
-                <a class="py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
+                <a class="button py-2 px-4 mt-4 inline-block bg-button text-gray rounded-xl" href="">Get started</a>
             </div>
         </div>
     </section>
 </template>
+
+<style scoped>
+.parent:hover .button{
+    background: #2D9CDB;
+    color: white;
+}
+</style>
